98f7ddfc6fa507034e4aaca353ffdf5ee8d13d1a
Arina <ashevkhod@yandex.ru>
2023-06-16 22:24:16 +0300

    -style=Mozilla with rebase

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,10 @@
 #include <string>
 
-int main() {
- 	std::string name; // var for name 
- 	std::getline(std::cin, name); // read line
- 	std::cout << "Hello World from " << name << std::endl; // print the resault 
- 	return 0;
+int
+main()
+{
+  std::string name;             // var for name
+  std::getline(std::cin, name); // read line
+  std::cout << "Hello World from " << name << std::endl; // print the resault
+  return 0;
 }
